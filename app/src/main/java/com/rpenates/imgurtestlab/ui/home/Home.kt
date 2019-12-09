73696d9455702d8b045e14287b527e5eb0c91fd2
Andres Peñates <andres.penates@gmail.com>
2019-12-09 16:57:33 -0500

    Implement search and list data

--- app/src/main/java/com/rpenates/imgurtestlab/ui/home/Home.kt
+++ app/src/main/java/com/rpenates/imgurtestlab/ui/home/Home.kt
@@ -1,13 +1,19 @@
 package com.rpenates.imgurtestlab.ui.home
 
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rpenates.imgurtestlab.R
 import com.rpenates.imgurtestlab.core.DI
+import com.rpenates.imgurtestlab.data.models.Photo
+import com.rpenates.imgurtestlab.ui.adapters.PhotoItemAdapter
 
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.content_home.*
 
 class Home : AppCompatActivity() {
 
@@ -22,6 +28,25 @@ class Home : AppCompatActivity() {
 
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
+        var listAdapter = PhotoItemAdapter()
+        photo_list.layoutManager = LinearLayoutManager(this)
+
+        viewModel.photosLiveData.observe(this, Observer {
+            listAdapter.photoList = it as ArrayList<Photo>
+            photo_list.adapter = listAdapter
+            listAdapter.notifyDataSetChanged()
+            if (it.isEmpty()) {
+                Toast.makeText(this, resources.getString(R.string.no_results),Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        search_button.setOnClickListener {
+            if (search_src_text.text.isEmpty()) {
+                Toast.makeText(this, resources.getString(R.string.empty_textfield), Toast.LENGTH_SHORT).show()
+            } else {
+                viewModel.searchPhotos(search_src_text.text.toString())
+            }
+        }
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
