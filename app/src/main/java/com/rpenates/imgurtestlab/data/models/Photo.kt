6cba2dc260c2e3c49a0b7f62b75090ec0b936107
Andres Peñates <andres.penates@gmail.com>
2019-12-13 18:51:15 -0500

    Delete unused model fields

--- app/src/main/java/com/rpenates/imgurtestlab/data/models/Photo.kt
+++ app/src/main/java/com/rpenates/imgurtestlab/data/models/Photo.kt
@@ -1,13 +1,11 @@
 package com.rpenates.imgurtestlab.data.models
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "photos")
 data class Photo (
     @PrimaryKey val id: String,
-    val parentAlbumId: String,
     val photoUrl: String,
     val title: String,
     val upvotes: Int,
@@ -16,6 +14,6 @@ data class Photo (
 ) {
     var isSelected: Boolean = false
     override fun toString(): String {
-        return "Photo(id='$id', parentAlbumId='$parentAlbumId', photoUrl='$photoUrl', title='$title')"
+        return "Photo(id='$id', photoUrl='$photoUrl', title='$title')"
     }
 }
